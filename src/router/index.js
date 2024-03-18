12c5cd66839d05b27eee8ca4c1305c77bd353944
nguyenthanhduy <duy959398@gmail.com>
2024-03-18 14:42:54 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,37 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+import NotFound from "@/views/NotFound.vue"; // Import NotFound component
+import ContactEdit from "@/views/ContactEdit.vue"; // Import ContactEdit component
+import ContactAdd from "@/views/ContactAdd.vue"; // Import ContactAdd component
+
 const routes = [
-{
-path: "/",
-name: "contactbook",
-component: ContactBook,
-    },
-    {
-path: "/:pathMatch(.*)*",
-name: "notfound",
-component: () => import("@/views/NotFound.vue"),
-},
+  {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
+  {
+    path: "/contacts/:id/edit",
+    name: "contact.edit",
+    component: ContactEdit,
+    props: true // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/contacts/add",
+    name: "contact.add",
+    component: ContactAdd,
+    props: true // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: NotFound, // Use the imported NotFound component
+  },
 ];
+
 const router = createRouter({
-history: createWebHistory(import.meta.env.BASE_URL),
-routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
+
 export default router;
