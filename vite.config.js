ee2ec50845c725fd39102fc96dfcbe72267a95c7
nguyenthanhduy <duy959398@gmail.com>
2024-03-18 14:15:43 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,14 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-port: 3001,
-},
+    port: 3001,
+  },
+  proxy: {
+    "/api": {
+      target: "http://localhost:3000/",
+      changeOrigin: true,
+    },
+  },
   plugins: [
     vue(),
     VueDevTools(),
@@ -18,4 +24,4 @@ port: 3001,
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
